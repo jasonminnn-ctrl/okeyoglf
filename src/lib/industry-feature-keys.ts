34a7db92ff5cfb90654cb03560d0ba2939876cc2
jasonminnn-ctrl <jasonminnn@gmail.com>
2026-03-11 23:59:01 +0900

    Create industry-feature-keys.ts

---
+++ src/lib/industry-feature-keys.ts
@@ -0,0 +1,155 @@
+export const INDUSTRY_FEATURE_KEYS = {
+  // 운영팀 - 골프연습장
+  OPERATIONS_INDOOR_PASS_MANAGEMENT: "operations.indoor.pass_management",
+  OPERATIONS_INDOOR_MEMBER_MANAGEMENT: "operations.indoor.member_management",
+  OPERATIONS_INDOOR_PRO_MANAGER_MANAGEMENT:
+    "operations.indoor.pro_manager_management",
+  OPERATIONS_INDOOR_LESSON_MANAGEMENT: "operations.indoor.lesson_management",
+  OPERATIONS_INDOOR_BAY_MANAGEMENT: "operations.indoor.bay_management",
+  OPERATIONS_INDOOR_LOCKER_MANAGEMENT: "operations.indoor.locker_management",
+
+  // 운영팀 - 골프장
+  OPERATIONS_COURSE_RESERVATION_TEETIME_MANAGEMENT:
+    "operations.course.reservation_teetime_management",
+  OPERATIONS_COURSE_OPERATION_ASSIGNMENT_MANAGEMENT:
+    "operations.course.operation_assignment_management",
+  OPERATIONS_COURSE_COURSE_FIELD_MANAGEMENT:
+    "operations.course.course_field_management",
+  OPERATIONS_COURSE_LOCKER_FACILITY_MANAGEMENT:
+    "operations.course.locker_facility_management",
+  OPERATIONS_COURSE_GROUP_EVENT_OPERATIONS:
+    "operations.course.group_event_operations",
+  OPERATIONS_COURSE_OPERATION_INSPECTION_MANAGEMENT:
+    "operations.course.operation_inspection_management",
+
+  // 운영팀 - 골프아카데미
+  OPERATIONS_ACADEMY_EDUCATION_PRODUCT_MANAGEMENT:
+    "operations.academy.education_product_management",
+  OPERATIONS_ACADEMY_STUDENT_MANAGEMENT:
+    "operations.academy.student_management",
+  OPERATIONS_ACADEMY_COACH_INSTRUCTOR_MANAGEMENT:
+    "operations.academy.coach_instructor_management",
+  OPERATIONS_ACADEMY_CLASS_SCHEDULE_MANAGEMENT:
+    "operations.academy.class_schedule_management",
+  OPERATIONS_ACADEMY_ATTENDANCE_MAKEUP_MANAGEMENT:
+    "operations.academy.attendance_makeup_management",
+  OPERATIONS_ACADEMY_FACILITY_BAY_MANAGEMENT:
+    "operations.academy.facility_bay_management",
+
+  // 운영팀 - 골프샵
+  OPERATIONS_SHOP_PRODUCT_MANAGEMENT: "operations.shop.product_management",
+  OPERATIONS_SHOP_INVENTORY_MANAGEMENT: "operations.shop.inventory_management",
+  OPERATIONS_SHOP_INBOUND_OUTBOUND_PROCUREMENT_MANAGEMENT:
+    "operations.shop.inbound_outbound_procurement_management",
+  OPERATIONS_SHOP_ORDER_DELIVERY_MANAGEMENT:
+    "operations.shop.order_delivery_management",
+  OPERATIONS_SHOP_STORE_DISPLAY_MANAGEMENT:
+    "operations.shop.store_display_management",
+  OPERATIONS_SHOP_CS_RETURNS_MANAGEMENT:
+    "operations.shop.cs_returns_management",
+
+  // 운영팀 - 피팅샵
+  OPERATIONS_FITTING_RESERVATION_MANAGEMENT:
+    "operations.fitting.reservation_management",
+  OPERATIONS_FITTING_FITTER_COUNSELOR_MANAGEMENT:
+    "operations.fitting.fitter_counselor_management",
+  OPERATIONS_FITTING_FITTING_SESSION_MANAGEMENT:
+    "operations.fitting.fitting_session_management",
+  OPERATIONS_FITTING_EQUIPMENT_INVENTORY_MANAGEMENT:
+    "operations.fitting.equipment_inventory_management",
+  OPERATIONS_FITTING_RESULT_RECORD_MANAGEMENT:
+    "operations.fitting.result_record_management",
+  OPERATIONS_FITTING_NO_SHOW_FOLLOWUP_MANAGEMENT:
+    "operations.fitting.no_show_followup_management",
+
+  // 운영팀 - 골프회사
+  OPERATIONS_COMPANY_ACCOUNT_MANAGEMENT:
+    "operations.company.account_management",
+  OPERATIONS_COMPANY_PARTNER_MANAGEMENT:
+    "operations.company.partner_management",
+  OPERATIONS_COMPANY_PRODUCT_BRAND_OPERATIONS:
+    "operations.company.product_brand_operations",
+  OPERATIONS_COMPANY_PROJECT_CAMPAIGN_OPERATIONS:
+    "operations.company.project_campaign_operations",
+  OPERATIONS_COMPANY_CHANNEL_OPERATION_STATUS:
+    "operations.company.channel_operation_status",
+  OPERATIONS_COMPANY_PROGRESS_STATUS_MANAGEMENT:
+    "operations.company.progress_status_management",
+
+  // 영업팀 - 골프연습장
+  SALES_INDOOR_PRICING: "sales.indoor.pricing",
+  SALES_INDOOR_KPI_MANAGEMENT: "sales.indoor.kpi_management",
+  SALES_INDOOR_REREGISTRATION_MANAGEMENT:
+    "sales.indoor.reregistration_management",
+  SALES_INDOOR_INACTIVE_VISITOR_MANAGEMENT:
+    "sales.indoor.inactive_visitor_management",
+  SALES_INDOOR_RESPONSE_SCRIPT: "sales.indoor.response_script",
+  SALES_INDOOR_CONSULTATION_MANAGEMENT:
+    "sales.indoor.consultation_management",
+
+  // 영업팀 - 골프장
+  SALES_COURSE_PRICING: "sales.course.pricing",
+  SALES_COURSE_UNSOLD_TEETIME_SALES: "sales.course.unsold_teetime_sales",
+  SALES_COURSE_CHANNEL_OTA_MANAGEMENT:
+    "sales.course.channel_ota_management",
+  SALES_COURSE_PACKAGE_GROUP_PROPOSAL:
+    "sales.course.package_group_proposal",
+  SALES_COURSE_KPI_MANAGEMENT: "sales.course.kpi_management",
+  SALES_COURSE_RESPONSE_SCRIPT: "sales.course.response_script",
+
+  // 영업팀 - 골프아카데미
+  SALES_ACADEMY_TRIAL_CONVERSION: "sales.academy.trial_conversion",
+  SALES_ACADEMY_REREGISTRATION_MANAGEMENT:
+    "sales.academy.reregistration_management",
+  SALES_ACADEMY_CHURN_ABSENCE_MANAGEMENT:
+    "sales.academy.churn_absence_management",
+  SALES_ACADEMY_PRICING: "sales.academy.pricing",
+  SALES_ACADEMY_KPI_MANAGEMENT: "sales.academy.kpi_management",
+  SALES_ACADEMY_CONSULTATION_MANAGEMENT:
+    "sales.academy.consultation_management",
+
+  // 영업팀 - 골프샵
+  SALES_SHOP_INQUIRY_MANAGEMENT: "sales.shop.inquiry_management",
+  SALES_SHOP_PURCHASE_CONVERSION_MANAGEMENT:
+    "sales.shop.purchase_conversion_management",
+  SALES_SHOP_BASKET_SIZE_PROPOSAL: "sales.shop.basket_size_proposal",
+  SALES_SHOP_VIP_REGULAR_MANAGEMENT:
+    "sales.shop.vip_regular_management",
+  SALES_SHOP_KPI_MANAGEMENT: "sales.shop.kpi_management",
+  SALES_SHOP_RESPONSE_SCRIPT: "sales.shop.response_script",
+
+  // 영업팀 - 피팅샵
+  SALES_FITTING_CONSULTATION_MANAGEMENT:
+    "sales.fitting.consultation_management",
+  SALES_FITTING_PURCHASE_CONVERSION_MANAGEMENT:
+    "sales.fitting.purchase_conversion_management",
+  SALES_FITTING_BRAND_PRODUCT_PROPOSAL:
+    "sales.fitting.brand_product_proposal",
+  SALES_FITTING_PRICING: "sales.fitting.pricing",
+  SALES_FITTING_KPI_MANAGEMENT: "sales.fitting.kpi_management",
+  SALES_FITTING_RESPONSE_SCRIPT: "sales.fitting.response_script",
+
+  // 영업팀 - 골프회사
+  SALES_COMPANY_LEAD_MANAGEMENT: "sales.company.lead_management",
+  SALES_COMPANY_PROPOSAL_MANAGEMENT:
+    "sales.company.proposal_management",
+  SALES_COMPANY_QUOTATION_PRICING:
+    "sales.company.quotation_pricing",
+  SALES_COMPANY_NO_RESPONSE_FOLLOWUP_MANAGEMENT:
+    "sales.company.no_response_followup_management",
+  SALES_COMPANY_KPI_MANAGEMENT: "sales.company.kpi_management",
+  SALES_COMPANY_RESPONSE_SCRIPT: "sales.company.response_script",
+
+  // 마케팅팀 공통
+  MARKETING_COPY: "marketing.copy",
+  MARKETING_EVENT: "marketing.event",
+  MARKETING_PROMOTION: "marketing.promotion",
+  MARKETING_SEASON_CAMPAIGN: "marketing.season_campaign",
+  MARKETING_CHANNEL_STATUS: "marketing.channel_status",
+  MARKETING_CONTENT_GENERATOR: "marketing.content_generator",
+} as const;
+
+export type IndustryFeatureKey =
+  (typeof INDUSTRY_FEATURE_KEYS)[keyof typeof INDUSTRY_FEATURE_KEYS];
+
+export const industryFeatureKeyList = Object.values(INDUSTRY_FEATURE_KEYS);
